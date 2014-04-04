916c78d2ce4b1ae26abb967da1ebc63c0363e339
Josh Bruce <josh@surftrack.co.uk>
2014-04-04 14:31:42 +0100

    Initial project, empty iOS application with name as application name.

---
+++ Josh Bruce/Josh Bruce/JoshBruceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JoshBruceAppDelegate.h
+//  Josh Bruce
+//
+//  Created by Josh Bruce on 04/04/2014.
+//  Copyright (c) 2014 Josh Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JoshBruceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Josh Bruce/Josh Bruce/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Josh Bruce
+//
+//  Created by Josh Bruce on 04/04/2014.
+//  Copyright (c) 2014 Josh Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JoshBruceAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JoshBruceAppDelegate class]));
+    }
+}
