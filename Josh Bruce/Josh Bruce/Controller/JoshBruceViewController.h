30f15bd2e6a452cd5f1310ed85436e9ecab76a25
Josh Bruce <josh@surftrack.co.uk>
2014-04-04 14:39:09 +0100

    Add the first view and its controller as well as setting the main storyboard for iPhone

---
+++ Josh Bruce/Josh Bruce/Controller/JoshBruceViewController.h
@@ -0,0 +1,13 @@
+//
+//  JoshBruceViewController.h
+//  Josh Bruce
+//
+//  Created by Josh Bruce on 04/04/2014.
+//  Copyright (c) 2014 Josh Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JoshBruceViewController : UIViewController
+
+@end
