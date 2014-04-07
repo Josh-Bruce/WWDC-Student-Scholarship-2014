f075be8c371e9585b22aecefd3b1488e8b4aeaae
Josh Bruce <josh@surftrack.co.uk>
2014-04-07 09:52:07 +0100

    Added a map view for my location and improved the about me view controller.

--- Josh Bruce/Josh Bruce/Controller/AboutMeViewController.m
+++ Josh Bruce/Josh Bruce/Controller/AboutMeViewController.m
@@ -7,17 +7,62 @@
 //
 
 #import "AboutMeViewController.h"
+#import "RoundedRect.h"
+#import <QuartzCore/QuartzCore.h>
+#import <MapKit/MapKit.h>
 
-@interface AboutMeViewController ()
-
+@interface AboutMeViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet RoundedRect *ageView;
+@property (weak, nonatomic) IBOutlet RoundedRect *locationView;
+@property (weak, nonatomic) IBOutlet UILabel *ageSwipeToContinue;
+@property (weak, nonatomic) IBOutlet MKMapView *locationMapView;
 @end
 
 @implementation AboutMeViewController
 
+#define VIEW_CENTRE_X 160
+
+#define WELCOME_START_X 160
+#define WELCOME_START_Y 62
+#define WELCOME_FINISH_X 160
+#define WELCOME_FINISH_Y 262
+
+#define ALPHA_START 0.0
+#define ALPHA_FINISH 1.0
+
+#define ANIMATION_DURATION 1.0
+
+#define LOCATION_LATITUDE 50.379853
+#define LOCATION_LONGITUDE -4.137757
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.locationMapView.layer.cornerRadius = 10.0;
+    self.locationMapView.layer.masksToBounds = YES;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGPoint newPosition = CGPointMake(scrollView.contentOffset.x + VIEW_CENTRE_X, self.ageSwipeToContinue.layer.position.y);
+    self.ageSwipeToContinue.layer.position = newPosition;
+    
+    if (scrollView.contentOffset.x == 320 && self.locationView.alpha != 1.0) {
+        // Fade in the location view and set the coordinates of where I live
+        [UIView animateWithDuration:ANIMATION_DURATION animations:^{
+			CGPoint newPosition = CGPointMake(WELCOME_FINISH_X, WELCOME_FINISH_Y);
+			self.locationView.layer.position = newPosition;
+			self.locationView.alpha = ALPHA_FINISH;
+		} completion:^(BOOL finished) {
+			MKPointAnnotation *myLocation = [[MKPointAnnotation alloc] init];
+			myLocation.coordinate = CLLocationCoordinate2DMake(LOCATION_LATITUDE, LOCATION_LONGITUDE);
+			myLocation.title = @"Josh Bruce's Home";
+			[self.locationMapView addAnnotation:myLocation];
+			[self.locationMapView showAnnotations:@[myLocation] animated:YES];
+		}];
+    }
 }
 
 - (IBAction)swipeUpMainMenu:(UISwipeGestureRecognizer *)sender
